4cedc488812639e254de4b88a26b84f48bac5ab2
gisele <medina.g.contact@gmail.com>
2026-03-24 16:42:53 -0300

    feat: create endpoint idea post

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,8 +3,10 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { type Application } from 'express';
 
-export const app: Application = express();
+//routes
+import v1Routes from './routes.js';
 
+export const app: Application = express();
 //cors
 app.use(cors());
 
@@ -15,12 +17,14 @@ app.use(express.json());
 // Parse URL-encoded bodies (extended: true allows nested objects)
 app.use(express.urlencoded({ extended: true }));
 
+//routes
+app.use(v1Routes);
 //routes health
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({
     message: 'APP is live',
     date: new Date().toISOString(),
-    version:"1.0.0",
-    github:"https://github.com/code-medina/brain-second-back.git"
+    version: '1.0.0',
+    github: 'https://github.com/code-medina/brain-second-back.git',
   });
 });
