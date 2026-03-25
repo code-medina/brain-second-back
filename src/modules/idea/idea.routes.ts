7a073ee5f5badaaadc38daea4e32f0b2eaaa3418
gisele <medina.g.contact@gmail.com>
2026-03-25 15:43:15 -0300

    feat:add idea controller method postIdea

--- src/modules/idea/idea.routes.ts
+++ src/modules/idea/idea.routes.ts
@@ -1,20 +1,11 @@
-import crypto from "node:crypto";
-
 import { Router } from 'express';
+
+import { IdeaController } from "./idea.controller.js";
+import { IdeaService } from "./idea.service.js";
 
 export const ideaRoutes = Router();
 
-ideaRoutes.post('/ideas', (req, res, next) => {
-  console.log('idea');
-  const _id=crypto.randomUUID();
-  const { title, description } = req.body as {
-    title: string;
-    description: string;
-  };
+const service=new IdeaService()
+const controller=new IdeaController(service);
 
-  if (!title) return next(new Error('title is required'));
-
-  const newIdea = { _id, title, description ,createdAt:new Date().toISOString()};
-
-  res.json({ message: 'create idea successfully', data: newIdea });
-});
+ideaRoutes.post('/ideas', controller.postIdea);
