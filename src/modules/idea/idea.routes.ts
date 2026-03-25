275edf867c44ed0e5d74ec06b29d09342a03bc34
gisele <medina.g.contact@gmail.com>
2026-03-25 15:15:13 -0300

    refactor add id and createdat in new idea mock

--- src/modules/idea/idea.routes.ts
+++ src/modules/idea/idea.routes.ts
@@ -1,9 +1,12 @@
+import crypto from "node:crypto";
+
 import { Router } from 'express';
 
 export const ideaRoutes = Router();
 
 ideaRoutes.post('/ideas', (req, res, next) => {
   console.log('idea');
+  const _id=crypto.randomUUID();
   const { title, description } = req.body as {
     title: string;
     description: string;
@@ -11,7 +14,7 @@ ideaRoutes.post('/ideas', (req, res, next) => {
 
   if (!title) return next(new Error('title is required'));
 
-  const newIdea = { id: '1', title, description };
+  const newIdea = { _id, title, description ,createdAt:new Date().toISOString()};
 
   res.json({ message: 'create idea successfully', data: newIdea });
 });
