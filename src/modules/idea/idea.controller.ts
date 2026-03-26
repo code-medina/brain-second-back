3fbb3977891c385a2e1128a2fa9fdbe811f322e1
gisele <medina.g.contact@gmail.com>
2026-03-25 21:32:51 -0300

    feat: add schema and dto to create idea

--- src/modules/idea/idea.controller.ts
+++ src/modules/idea/idea.controller.ts
@@ -1,7 +1,7 @@
 import type { Request, Response, NextFunction } from 'express';
 
+import { CreateIdeaSchema} from './idea.schema.js';
 import type { IdeaService } from './idea.service.js';
-
 
 export class IdeaController {
   service: IdeaService;
@@ -10,18 +10,23 @@ export class IdeaController {
   }
   postIdea = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { title, description } = req.body as {
-        title: string;
-        description: string;
-      };
-      const newIdea = await this.service.createIdea({ title, description });
-      return res
-        .status(201)
-        .json({
-          message: 'Creited idea successfully',
-          ok: true,
-          data: newIdea,
-        });
+      const validData = CreateIdeaSchema.safeParse(req.body);
+      if (!validData.success) {
+        return res
+          .status(400)
+          .json({
+            message: 'invalid input to create idea',
+            ok: false,
+            data: validData.error.issues
+          });
+      }
+
+      const newIdea = await this.service.createIdea(validData.data);
+      return res.status(201).json({
+        message: 'Created idea successfully',
+        ok: true,
+        data: newIdea,
+      });
     } catch (error) {
       next(error);
     }
