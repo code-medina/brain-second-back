7efb0a5ca2415618b1b26c37884ff564d50add01
gisele <medina.g.contact@gmail.com>
2026-04-02 15:55:49 -0300

    feat: add endpoint delete idea

--- src/modules/idea/idea.routes.ts
+++ src/modules/idea/idea.routes.ts
@@ -4,9 +4,22 @@ import { ideaController } from './idea.module.js';
 import { CreateIdeaSchema, UpdateIdeaSchema } from './idea.schema.js';
 import { validateBody } from '../../middlewares/validate.middleware.js';
 
-
 export const ideaRoutes = Router();
 
-ideaRoutes.post('/ideas',validateBody(CreateIdeaSchema), ideaController.postIdea);
-ideaRoutes.get('/ideas',ideaController.getIdeas);
-ideaRoutes.put("/ideas",validateBody(UpdateIdeaSchema),ideaController.editIdea)
+ideaRoutes.post(
+  '/ideas',
+  validateBody(CreateIdeaSchema),
+  ideaController.postIdea,
+);
+ideaRoutes.get('/ideas', ideaController.getIdeas);
+ideaRoutes.put(
+  '/ideas',
+  validateBody(UpdateIdeaSchema),
+  ideaController.editIdea,
+);
+ideaRoutes.delete('/ideas/:id', (req, res) => {
+  const _id = req.params.id;
+  return res
+    .status(200)
+    .json({ ok: true, message: 'deleting successfully', data: _id });
+});
